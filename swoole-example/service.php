aeae3469bcbaf2cbc73e2a27c42794870dca823c
Allison Piovani <apiovani25@gmail.com>
2024-07-04 22:35:53 -0300

    inited: module fpm-example

--- swoole-example/service.php
+++ swoole-example/service.php
@@ -9,8 +9,8 @@ use Swoole\Http\Response;
 $server = new Server('0.0.0.0', 8080);
 
 $server->on('request', function (Request $request, Response $response) {
-    $response->header('Content-Type', 'text/html');
-    $response->end('<h1>Hello, World 1</h1>');
+    $response->header('Content-Type', 'application/json');
+    $response->end(json_encode(["status" => "ok"]));
 });
 
 $server->start();
